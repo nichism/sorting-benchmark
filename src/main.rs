6a3c94e618022e46195bc1141d42d08e14d55b3c
Liam Young <nichismokeyt@gmail.com>
2023-05-14 23:50:21 -0700

    trying to get it working with negative nums... not working yet.

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,68 @@
-
-
+use std::time::Instant;
 fn main() {
-    let static_arr = [3,1,2,5,4];
-    let mut arr = static_arr;
-
-    //let first = &arr[0..2];
-    //let second = &arr[3..arr.len()];
-    //println!("first: {:?}, second: {:?}", first, second);
-    do_quick_sort(&mut arr);
+    
+    //let static_arr = [3,1,2,5,4,8,62,-52,55,66,142,-65,-97,-77,-78,20,-600];
+    let static_arr = [3,1,2,5,4,-6,-77,50];
     println!("Static array: {:?} ", static_arr);
+    let mut arr = static_arr;
+    let timer = Instant::now();
+    do_quick_sort(&mut arr);
+    println!("do_quick_sort took: {:?} nanoseconds.", timer.elapsed().as_nanos());
     println!("Sorted array: {:?}", arr);
+    
 }
 
 fn do_quick_sort(arr: &mut [i32]) {
     if arr.len() == 1 {
         return;
     }
-    let pivot_index = arr.len()-1;
+    println!("do_quick_sort called with arr: {:?} ", arr);
     let pivot_value = arr[arr.len()-1];
-
-    println!("Pivot value: ");
     let mut current_index = 0;
-    let mut swap_index = 0;
+    let mut swap_index: isize = -1;
+    let mut first_pass: bool = true;
     while current_index < arr.len() {
-        let val = arr[current_index];
-        if val < pivot_value {
-            swap_index+=1;
-            if current_index > swap_index {
-                let temp_current_index_val = arr[current_index];
-                let temp_swap_index_val = arr[swap_index];
 
-                arr[current_index] = temp_swap_index_val;
-                arr[swap_index] = temp_current_index_val;
-                if val == pivot_value && current_index == pivot_index {
-                    let len = arr.len();
-                    let mut less_array = &mut arr[0..current_index];
-                    do_quick_sort(&mut less_array);
-                    let mut greater_array = &mut arr[current_index+1..len];
-                    do_quick_sort(&mut greater_array);
-                }
+        if !first_pass {
+            current_index+=1;
+            if current_index >= arr.len() {
+                break;
             }
         }
         
-        current_index+=1;
+        let current_index_value = arr[current_index];
+        println!("comparing ({:?})(idx: {:?}) to pivot_value ({:?})", current_index_value, current_index, pivot_value);
+        if current_index_value.lt(&pivot_value) || current_index_value.eq(&pivot_value) {
+            println!("current_index_value ({:?})(idx: {:?}) is less than or equal to pivot_value ({:?})", current_index_value, current_index, pivot_value);
+            swap_index+=1;
+            if current_index > swap_index as usize {
+                let temp_current_index_val = arr[current_index];
+                let temp_swap_index_val = arr[swap_index as usize];
+
+                arr[current_index] = temp_swap_index_val;
+                arr[swap_index as usize] = temp_current_index_val;
+                    
+                println!("Swapping current index {:?} with swap index {:?} ", current_index, swap_index);
+                if current_index == arr.len()-1 {
+                    let len = arr.len();
+                    println!("arr after swap: {:?}", arr);
+                    println!("VALUE IS: {:?}", arr[swap_index as usize]);
+                    let mut less_array = &mut arr[0..swap_index as usize];
+                    println!("less_array IS: {:?}", less_array);
+                    if less_array.len() > 0 {
+                        do_quick_sort(&mut less_array);
+                    }
+                    
+                    let mut greater_array = &mut arr[swap_index as usize +1..len];
+                    println!("greater_array IS: {:?}", greater_array);
+                    if greater_array.len() > 0 {
+                        do_quick_sort(&mut greater_array);
+                    }
+                }
+            }
+        }
+        if first_pass {
+            first_pass = false;
+        }
     }
 }
